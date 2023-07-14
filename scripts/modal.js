aa3692f597081ecadc2f7ac0918cc597a48f6c3c
moises.hilel <moises.hilel@gmail.com>
2023-07-13 23:53:24 -0300

    Adicionando arquivos do portifolio

---
+++ scripts/modal.js
@@ -0,0 +1,270 @@
+// Dados dos Projetos
+
+const dataProjects = [
+    {
+        title: 'Challenge Encriptador',
+        date: '25 de Junho de 2023',
+        description: `editando... <br><br>
+
+        editando... <br><br>
+        
+        • editando...; <br>
+        • editando...; <br>
+        • editando...; <br>
+        • editando...; <br>
+        • editando...`, 
+        videoSrc: 'assets/projects/encriptador.jpg',
+        linkedin: '',
+        repository: 'https://github.com/moiseshilel/challenge-Encriptador',
+        site: 'https://moiseshilel.github.io/challenge-Encriptador/'
+
+    },
+    {
+        title: 'Conversor de Moedas',
+        date: '2023',
+        description: `Foi proposto que fosse desenvolvido um conversor de moedas, com valores prefixados e um segundo conversor à escolha do aluno.<br><br>
+
+        Esse projeto foi desenvolvido com as seguintes ferramentas: <br><br>
+        
+        • Java; <br>
+        • Eclipse; <br>
+        `, 
+        videoSrc: 'assets/projects/conversor.mp4',
+        linkedin: '',
+        repository: 'https://github.com/moiseshilel/conversor-de-Moedas',
+        site: 'https://github.com/moiseshilel/conversor-de-Moedas'
+
+    },
+    {
+        title: 'Site Barbearia Alura',
+        date: '15 de Junho de 2023',
+        description: `editando...; <br><br>
+        • editando...; <br>
+        • editando...;' <br>
+        • editando...; <br>
+        • editando...; <br>
+        • editando...;`,
+        videoSrc: 'assets/projects/sitebarbearia.jpg',
+        linkedin: '',
+        repository: 'https://github.com/moiseshilel/site-Barbearia-Alura',
+        site: 'https://moiseshilel.github.io/site-Barbearia-Alura/'
+
+    },
+    {
+        title: 'Site Barbearia Alura',
+        date: '15 de Junho de 2023',
+        description: "---",
+        videoSrc: 'assets/projects/conversor.mp4',
+        linkedin: '',
+        repository: 'https://github.com/moiseshilel/site-Barbearia-Alura',
+        site: 'https://moiseshilel.github.io/site-Barbearia-Alura/'
+
+    },
+    {
+        title: 'Youtube Clone',
+        date: '23 de Setembro de 2021',
+        description: "Essa foi a minha tentativa de clonar a interface do YouTube utilizando HTML5, CSS3 e JavaScript. Com o conceito Don't Repeat Yourself na mente, fiz com que todo o projeto rodasse em uma única página manipulada pelo JavaScript, que fica responsável por atualizar o source e a descrição dos vídeos dependendo de qual vídeo o usuário seleciona, além de ocultar/exibir o sidebar e a main para dar destaque ao vídeo. Estou há aproximadamente um mês e meio estudando JavaScript e estou bem satisfeito por já estar conseguindo utilizá-lo para facilitar o desenvolvimento dos meus projetos, isso é extremamente motivador. Voltarei nesse projeto em breve para aprimorá-lo cada vez mais.",
+        videoSrc: 'https://streamable.com/e/q700dy',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_html-css-developer-activity-6845803020157116417-39dj',
+        repository: 'https://github.com/rhuanbello/clone-youtube',
+        site: 'https://youtube-rhuanbello.vercel.app/'
+
+    },
+    {
+        title: 'Digital Flix',
+        date: 'Em desenvolvimento',
+        description: "Em desenvolvimento",
+        videoSrc: '',
+        linkedin: '',
+        repository: '',
+        site: ''
+
+    },
+    {
+        title: 'Barber House',
+        date: '30 de Julho de 2021',
+        description: "A Barber House é uma landing page mobile-first que reúne os conceitos fundamentais de HTML5, CSS3 e JavaScript. Ela possui animações para fazer surgir gradualmente cada section no decorrer do scroll. Além de ser totalmente responsiva para diferentes tamanhos de tela, é uma página facilmente customizável, pois todas as cores utilizadas estão armazenadas em variáveis dentro do CSS.",
+        videoSrc: 'https://streamable.com/e/6ci737',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_opentowork-css-html-activity-6827021119288156160-qT7i',
+        repository: 'https://rhuanbello.github.io/nlw6-origin/',
+        site: 'https://rhuanbello.github.io/nlw6-origin/'
+
+    },
+    {
+        title: 'Rocket.Q',
+        date: '07 de Julho de 2021',
+        description: "Minha primeira participação da Next Leve Week da Rocketseat. <br><br> Desenvolvemos do zero uma aplicação de gerenciamento de perguntas anônimas, esse foi meu primeiro contato com o Node.js, que apesar de não ser o meu foco, foi extremamente interessante conhecer.",
+        videoSrc: 'https://streamable.com/e/4ozfu5',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_opentowork-css-html-activity-6818692643816665088-FxZJ',
+        repository: 'https://github.com/rhuanbello/nlw6-discover',
+        site: ''
+
+    },
+    {
+        title: 'Naped',
+        date: 'Em desenvolvimento',
+        description: "Em desenvolvimento",
+        videoSrc: 'https://streamable.com/e/u6x84w',
+       
+        linkedin: '',
+        repository: 'https://github.com/rhuanbello/naped',
+        site: 'https://naped-rhuanbello.vercel.app/'
+
+    },
+    {
+        title: 'Mercado Livre',
+        date: 'Em desenvolvimento',
+        description: "Em desenvolvimento",
+        videoSrc: 'https://streamable.com/e/dyteys',
+        linkedin: '',
+        repository: 'https://github.com/rhuanbello/clone-mercadoLivre',
+        site: 'https://mercadolivre-rhuanbello.vercel.app/'
+
+    },
+    {
+        title: 'Rio Turism',
+        date: '04 de Setembro de 2021',
+        description: "Esse é um projeto que realizei para o meu primeiro checkpoint da disciplina Front-End I da formação Certified Tech Developer na Digital House Brasil, em parceria com Mercado Livre e a Globant. <br><br> Neste checkpoint foi pedido um página sobre minha cidade, onde pudesse ser praticado os conceitos da propriedade position do CSS3. Aqui foi bem interessante trabalhar com contraste de cores e altura de linha para oferecer uma boa leitura ao usuário, além de praticar posicionamento sem precisar necessariamente de Flexbox ou CSS Grid Layout.",
+        videoSrc: 'https://streamable.com/e/cwyvwh',
+        linkedin: '',
+        repository: 'https://github.com/rhuanbello/rioturism',
+        site: 'https://rioturism-rhuanbello.vercel.app/'
+
+    },
+    {
+        title: 'Cultflix',
+        date: '30 de Julho de 2021',
+        description: "Cultflix é um projeto que nasce desse fascínio e me convida a pôr em prática todo o aprendizado adquirido no Bootcamp HTML Web Developer da Digital Innovation One Inc. Ter a oportunidade de criar a minha própria versão de uma Plataforma de Streaming e ter a liberdade de inserir meus filmes favoritos é extremamente gratificante.",
+        videoSrc: 'https://streamable.com/e/1tsfrm',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_ui-opentowork-css-activity-6821251240840835072-6GP0',
+        repository: 'https://github.com/rhuanbello/cultflix',
+        site: 'https://rhuanbello.github.io/cultflix/'
+
+    },
+    
+    {
+        title: 'Dashboard Star Wars',
+        date: '08 de Julho de 2021',
+        description: "Reunindo dois universos que eu amo: cinema e desenvolvimento. Assim, os estudos se tornam realmente divertidos! <br><br> Dashboard Star Wars é um projeto que realiza requisições de uma API externa para trazer diversos dados sobre o universo da saga. Projeto desenvolvido em um dia, apenas para praticar alguns conceitos.",
+        videoSrc: 'https://streamable.com/e/84kzet',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_desenvolvimento-ui-starwars-activity-6818976321042038784-FPYl',
+        repository: 'https://github.com/rhuanbello/clone-windowsCalculator',
+        site: 'https://rhuanbello.github.io/dashboard-starwars/'
+
+    },
+    {
+        title: 'Animais Fantásticos',
+        date: 'Em desenvolvimento',
+        description: "Em desenvolvimento",
+        videoSrc: 'https://streamable.com/e/5ugnu0',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_desenvolvimento-ui-starwars-activity-6818976321042038784-FPYl',
+        repository: 'https://github.com/rhuanbello/animais-fantasticos',
+        site: 'https://animaisfantasticos.vercel.app/'
+
+    },
+    {
+        title: 'Brafé',
+        date: '07 de Agosto de 2021',
+        description: "Projeto desenvolvido durante o curso de CSS Avançado da Origamid, com duração de 9 horas. <br><br> Ao longo do curso colocamos em prática de forma exaustiva os principais métodos de posicionamento no CSS, desenvolvendo a mesma página de quatro formas diferentes.",
+        videoSrc: 'https://streamable.com/e/wkskpg',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_opentowork-css-bootstrap-activity-6829784614278713344-j3x8',
+        repository: 'https://github.com/rhuanbello/brafe',
+        site: 'https://brafe-rhuanbello.vercel.app/'
+
+    },
+    {
+        title: 'Bikcraft',
+        date: '15 de Junho de 2021',
+        description: "Projeto final do ótimo curso de Web Design Completo da Origamid, onde tive a oportunidade de desenvolver do wireframe ao código o projeto Bikcraft, colocando em prática todos os conteúdos teóricos apresentados no curso, como: UX/UI Design, Adobe XD, Semântica, SEO, Grid, Reset, Responsividade, Animações, Domínio, hospedagem e muito mais.",
+        videoSrc: 'https://streamable.com/e/rtxzgb',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_opentowork-css-html-activity-6810692015794331648-f2UO',
+        repository: 'https://github.com/rhuanbello/bikcraft',
+        site: 'https://rhuanbello.github.io/bikcraft/'
+
+    },
+    {
+        title: 'Flexbog',
+        date: '17 de Junho de 2021',
+        description: "Projeto final do curso de CSS FlexBox da Origamid, um layout que te facilita bastante na hora definir o tamanho e o alinhamento (vertical e horizontal) de itens. <br><br> Apesar de ser uma página simples, ela engloba as mais importantes propriedades do CSS FlexBox, como o Display Flex, Flex Wrap, Justify Content, Flex Basis, etc. Sendo assim, depois de alguns pequenos ajustes, a página já se torna completamente responsiva, pois o FlexBox é um layout bastante inteligente por si só.",
+        videoSrc: 'https://streamable.com/e/d3c1bk',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_opentowork-css-html-activity-6811412518758707200-jcJ8',
+        repository: 'https://github.com/rhuanbello/flexblog',
+        site: 'https://rhuanbello.github.io/flexblog/'
+
+    },
+    {
+        title: 'Le Scone',
+        date: '01 de Outubro de 2021',
+        description: "Projeto desenvolvido durante o curso de CSS com SASS da Origamid.",
+        videoSrc: 'https://streamable.com/e/ajfxe4',
+        linkedin: '',
+        repository: 'https://github.com/rhuanbello/lesconde',
+        site: 'https://lesconde-rhuanbello.vercel.app/'
+
+    },
+    {
+        title: 'Wildbeast',
+        date: '09 de Julho de 2021',
+        description: "Projeto de mais um curso da Origamid, dessa vez sobre CSS Grid Layout, que sinceramente é uma mão na roda. Nunca foi tão fácil mudar totalmente a composição de um site, você literalmente pode fazer isso com uma linha de código. Após conhecer todos os conceitos dessa especificação (como o Grid Template Columns, Rows, Areas e Auto) foi desenvolvido o site Wildbeast para fixar na prática todo o aprendizado.",
+        videoSrc: 'https://streamable.com/e/dyan89',
+        linkedin: 'https://www.linkedin.com/posts/rhuanbello_opentowork-css-html-activity-6819437498171822080-BTlV',
+        repository: 'https://github.com/rhuanbello/wildbeast',
+        site: 'https://rhuanbello.github.io/wildbeast/'
+
+    },
+
+]
+
+// Open Modal
+
+const openModal = () => {
+    const closeModal = document.querySelector('.modal .ri-close-line')
+    const modal = document.querySelector('.modal')
+    const box = document.querySelectorAll('.cards-projects .box')
+    const modalTitle = modal.querySelector('.title h2')
+    const modalDescription = modal.querySelector('.info p')
+    const modalDate = modal.querySelector('span')
+    const modalLinkProject = modal.querySelector('.links a.link-project')
+    const modalLinkRepository = modal.querySelector('.links a.link-repository')
+    const modalLinkLinkedin = modal.querySelector('.links a.link-linkedin')
+    const iframe = modal.querySelector('.video iframe')
+
+    box.forEach((item, index) => {
+
+        const addData = () => {
+
+            if (index != 5) {
+                modal.classList.remove('hidden');
+                document.body.style.overflow = 'hidden';
+                document.body.classList.add('transparent')
+            }
+
+            modalTitle.innerHTML = dataProjects[index].title
+            modalDescription.innerHTML = dataProjects[index].description
+            modalDate.innerHTML = dataProjects[index].date
+            modalLinkProject.setAttribute('href', dataProjects[index].site) 
+            modalLinkRepository.setAttribute('href', dataProjects[index].repository) 
+            modalLinkLinkedin.setAttribute('href', dataProjects[index].linkedin) 
+            iframe.setAttribute('src', dataProjects[index].videoSrc + '?autoplay=1&amp;loop=0')
+
+        }
+
+        item.addEventListener('click', addData)
+
+    })
+
+    const closingModal = () => {
+        modal.classList.add('hidden')
+        document.body.style.overflow = 'visible';
+        document.body.classList.remove('transparent')
+        iframe.setAttribute('src', '')
+
+    }
+
+    closeModal.addEventListener('click', closingModal)
+
+    document.addEventListener ('keydown', (event) => event.key === "Escape" ? closingModal() : '');
+
+}
+
+openModal()
